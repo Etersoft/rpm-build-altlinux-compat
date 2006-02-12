338d578c0bc329177318819991e3478a59fc3993
Vitaly Lipatov <lav@etersoft.ru>
2006-02-12 17:06:15 +0000

    update release script

--- release_rpm-build-altlinux-compat.sh
+++ release_rpm-build-altlinux-compat.sh
@@ -1,70 +1,64 @@
 #!/bin/sh
+# Release script for small projects packaged in RPM
+# Use etersoft-build-utils as helper
 . /etc/rpm/etersoft-build-functions
 
 NAME=rpm-build-altlinux-compat
-STEP=1
+PUBLICPATH=etersoft:~/download/$NAME
 
 SPECNAME=$NAME.spec
 build_rpms_name $SPECNAME
 TARNAME=$NAME-$VERSION.tar.bz2
 
-cd ..
-ln -s $NAME $NAME-$VERSION
-tar cvfj $TARNAME $NAME-$VERSION
-rm -f $NAME-$VERSION
-cd -
-exit 1
+STEP=2
 
-PUBLICPATH=etersoft:~/download/%NAME
-
-ssh-add -l || ssh-add
-
+# Update from CVS
 if [ $STEP -le 1 ]; then
-	echo "Step 1"
-	cd $NAME-$VER
-	cvs -z3 update -dPR || { echo ERROR; exit 1; }
-	./autogen.sh
-	rm -rf autom4te.cache
-	cd -
-	tar cvfj $TARNAME $NAME-$VER/* || exit 1
+	if [ -d CVS ] ; then
+		cvs -z3 update -dPR || fatal_error "Can't update from CVS..."
+	fi
 fi
 
+# Make tarball
 if [ $STEP -le 2 ]; then
-	echo "Step 2"
-	cp -f $TARNAME $RPMDIR/SOURCES/
+	cd ..
+	ln -s $NAME $NAME-$VERSION
+	tar cvfj $TARNAME $NAME-$VERSION/* || fatal_error "Can't create tarball"
+	rm -f $NAME-$VERSION
+	cd -
 fi
-#exit 1
+
+check_key
+
 
 if [ $STEP -le 3 ]; then
 	echo "Step 3"
-	scp $TARNAME cf.sf:~
+	cp -f ../$TARNAME $RPMDIR/SOURCES/
 fi
+
+#if [ $STEP -le 3 ]; then
+#	echo "Step 3"
+#	scp $TARNAME cf.sf:~
+#fi
 #exit 1
 
 if [ $STEP -le 4 ]; then
 	echo "Step 4"
-	cd $NAME-$VER
-	rpmbshr $SPECNAME 
-	cd -
+	rpmbb $SPECNAME 
 fi
 
-if [ $STEP -le 5 ]; then
-	echo "Step 5"
-	pushd $NAME-$VER
-	rpmbs -s $SPECNAME
-	# && cd $UPLOADDIR/.. && ./upload-to-alt
-	popd
-fi
+#if [ $STEP -le 5 ]; then
+#	echo "Step 5"
+#	rpmbs -s $SPECNAME
+#fi
 
 if [ $STEP -le 6 ]; then
 	echo "Step 6"
-	#ssh $BUILDSERVER ls $BUILDSERVERPATH/${NAME}-*${VER}-${REL}*
-	rsync --progress $TARNAME $PUBLICPATH/$TARNAME || exit 1
-	UDIR=$UPLOADDIR/../upload_alt_ftp/natspec
-	rsync --progress $TARNAME $UDIR || exit 1
-	#rm -rf $UDIR/libnatspec*
-	rsync --progress $BUILDSERVER:$BUILDSERVERPATH/${NAME}-*${VER}-${REL}* $UDIR/ \
-		&& cd $UPLOADDIR/.. && ./upload-to-alt-ftp
+	rsync --progress ../$TARNAME $PUBLICPATH/$TARNAME || fatal_error "Can't rsync"
+	#UDIR=$UPLOADDIR/../upload_alt_ftp/natspec
+	#rsync --progress $TARNAME $UDIR || exit 1
+	#rsync --progress $BUILDSERVER:$BUILDSERVERPATH/${NAME}-*${VER}-${REL}* $UDIR/ \
+	#	&& cd $UPLOADDIR/.. && ./upload-to-alt-ftp
 	#ftp.alt:/ftp/pub/people/lav/natspec
 	
 	#rsync --progress $BUILDSERVER:$BUILDSERVERPATH/${NAME}-*${VER}-${REL}* ~/tmp/
