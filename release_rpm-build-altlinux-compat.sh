c9d06a90d826b4985432e3d24b391c39387ae90c
Vitaly Lipatov <lav@etersoft.ru>
2008-10-30 12:03:43 +0000

    add release target support

--- release_rpm-build-altlinux-compat.sh
+++ release_rpm-build-altlinux-compat.sh
@@ -9,6 +9,17 @@ WORKDIR=/home/builder/Projects/eterbuild/functions
 
 test -f $WORKDIR/config.in && . $WORKDIR/config.in
 
+ALPHA=/current
+if [ "$1" = "-r" ] ; then
+	if [ -z "$2" ] ; then
+		ALPHA=/$WINENUMVERSION
+	else
+		ALPHA=/$2
+		shift
+	fi
+	shift
+fi
+
 check_key
 
 update_from_cvs
@@ -19,8 +30,8 @@ rpmbb $SPECNAME || fatal "Can't build"
 
 if [ "$WINEPUB_PATH" ] ; then
 	# Path to local publishing
-	ETERDESTSRPM=$WINEPUB_PATH/current/sources
-	publish_srpm
+	ETERDESTSRPM=$WINEPUB_PATH$ALPHA/sources
+	publish_srpm -z -s
 fi
 
 # Usual path to public sources
