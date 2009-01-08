b99afca8af0261b7cf4e5a09bb0d966e92734dd6
Vitaly Lipatov <lav@etersoft.ru>
2009-01-08 13:37:24 +0000

    update release script

--- release_rpm-build-altlinux-compat.sh
+++ release_rpm-build-altlinux-compat.sh
@@ -3,21 +3,14 @@
 # 2006, Public domain
 # Release script for small projects packaged in RPM
 # Use etersoft-build-utils as helper
+
+# if [ -r /usr/share/eterbuild/korinf ]
 . /etc/rpm/etersoft-build-functions
 
-WORKDIR=/home/builder/Projects/eterbuild/functions
-
-test -f $WORKDIR/config.in && . $WORKDIR/config.in
-
-ALPHA=/current
+ALPHA=/unstable
 if [ "$1" = "-r" ] ; then
-	if [ -z "$2" ] ; then
-		ALPHA=/$WINENUMVERSION
-	else
-		ALPHA=/$2
-		shift
-	fi
-	shift
+	ALPHA=/$2
+	shift 2
 fi
 
 check_key
@@ -31,13 +24,13 @@ rpmbb $SPECNAME || fatal "Can't build"
 if [ "$WINEPUB_PATH" ] ; then
 	# Path to local publishing
 	ETERDESTSRPM=$WINEPUB_PATH$ALPHA/sources
-	publish_srpm -z -s
+	publish_srpm -z
 fi
 
 # Usual path to public sources
 # scp $TARNAME cf.sf:~   (use below params for it)
-PUBLICSERVER=etersoft
-PUBLICPATH=/home/lav/download/$BASENAME
+#PUBLICSERVER=etersoft
+#PUBLICPATH=/home/lav/download/$BASENAME
 
-publish_tarball
+#publish_tarball
 
